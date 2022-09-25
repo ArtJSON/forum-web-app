3f1a1fed76e8fea08a4a7d6e965a099cb951817a
ArtJSON <artur.pietrzak1@outlook.com>
2022-09-25 20:38:36 +0200

    Add footer

--- components/layouts/Footer/Footer.tsx
+++ components/layouts/Footer/Footer.tsx
@@ -3,7 +3,24 @@ import Link from 'next/link';
 import styles from './Footer.module.scss';
 
 const Footer = () => {
-  return <footer className={styles.footer}></footer>;
+  return (
+    <footer className={styles.footer}>
+      <ul>
+        <li>
+          <Link href={'/about-us'}>About us</Link>
+        </li>
+        <li>
+          <Link href={'/contact'}>Contact</Link>
+        </li>
+      </ul>
+      <p className={styles.copyright}>
+        © 2022{' '}
+        <Link href={'https://arturpietrzak.com/'}>
+          <a className={styles.createdBy}>Artur Pietrzak</a>
+        </Link>
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
